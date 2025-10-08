f8c63f549c3f14fb498d00b339a73a7312ff9615
UHSR <uhsr@eteb.me>
2025-10-08 23:01:13 +0700

    Refactor index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,18 @@
 import { ZeroTestnetAutomationKitPro } from './zerotestnetautomationkitpro';
 import minimist from 'minimist';
 
+/**
+ * Define command line argument interface
+ */
 interface Args {
     verbose?: boolean;
     input?: string;
     output?: string;
 }
 
+/**
+ * Parse command line arguments
+ */
 const args: Args = minimist(process.argv.slice(2), {
     boolean: ['verbose'],
     alias: {
@@ -21,11 +27,20 @@ const args: Args = minimist(process.argv.slice(2), {
     }
 });
 
+/**
+ * Initialize application with parsed arguments
+ */
 const app = new ZeroTestnetAutomationKitPro({
     verbose: args.verbose
 });
 
+/**
+ * Execute application and handle any errors
+ */
 app.execute()
+    .then(() => {
+        process.exit(0); // exit with success code when execution is complete
+    })
     .catch(err => {
         console.error(err);
         process.exit(1);
